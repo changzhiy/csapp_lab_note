80f677d2c44d6f3bd6d527b5a59d700330c4f093
CV-Student <63215608+Cool-CoCoder@users.noreply.github.com>
2022-04-04 10:48:14 +0800

    Update bit_number.cpp

--- datalab/bit_number.cpp
+++ datalab/bit_number.cpp
@@ -1,3 +1,5 @@
+// 笔记只记录一些个人认为比较有价值的思路
+
 // 不用if else等语句， !x 可以将非0的数打印0, 而!0 为 1,而!!即可将非0返回1
 // 下方代码测试成功
 int32_t judgePostiveOrNegative(int32_t x){
@@ -22,6 +24,19 @@ int32_t isAsciiDigit(int32_t x){
     return !(upperBound|lowerBound);
 }
 
+// 某种意义上实现了if else操作, 我觉得非常巧妙, 利用了 全0 和 全 1 与另一个数的 & 操作.
+/*
+ * conditional - same as x ? y : z
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+    x = !!x;
+    x = ~x+1;
+    return (x&y)|(~x&z);
+}
 
 // 打印32位int变量
 void printBits(int32_t x){
